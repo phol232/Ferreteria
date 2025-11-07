defa78f68af496dbfbba8c0675be67e12bfea8da
Cuenta Secundaria <140575345+phol232@users.noreply.github.com>
2025-11-07 15:16:49 -0500

    Cambios

---
+++ Frontend/SocioConstructor/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://143.110.226.214:8000/api'
+};
--- proyectoSoftware/config/cors.php
+++ proyectoSoftware/config/cors.php
@@ -3,11 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'http://localhost:4200',
-        'http://127.0.0.1:4200',
-        '*'
-    ],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
